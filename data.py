4cf67a730da801adf261fe36addeb0e2e6e63b9e
thelivin <ayiteybobby@gmail.com>
2023-02-17 14:02:12 +0000

    first commit

---
+++ data.py
@@ -0,0 +1,6 @@
+fields = ["Software Engineering", "Cyber Security", "Data Science"]
+levels = ["Entry Level", "Intermediate", "Senior Level"]
+skills = [
+    "HTML", "CSS", "Python", "Java", "JavaScript",
+    "Swift", "C++", "C#", "R", "Golang (Go)", "Scala", "Kotlin"
+]
